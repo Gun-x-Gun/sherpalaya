bc3eb65d2ec63639c6a0d8f7582fd8cbadace922
Adesh Bhattarai <adeshbhattarai99@gmail.com>
2024-12-12 14:36:32 +0545

    enuns

--- app/Enums/ItineraryTypes.php
+++ app/Enums/ItineraryTypes.php
@@ -1,14 +1,30 @@
 <?php
 
 namespace App\Enums;
+use Filament\Support\Contracts\HasLabel;
 
-enum ItineraryTypes
+
+enum ItineraryTypes: string implements HasLabel
 {
     case FLIGHT = 'flight';
     case DRIVE = 'drive';
     case WALK = 'walk';
     case REST = 'rest';
-    case HOTEL = 'hotel';
+    case BED_BREAKFAST = 'bed_breakfast';
     case HIMALAYA = 'himalaya';
+    case ALTITUDE = 'altitude';
+
+    public function getLabel(): string
+    {
+        return match ($this){
+            self::FLIGHT => 'Flight',
+            self::DRIVE => 'Drive',
+            self::WALK => 'Walk',
+            self::REST => 'Rest',
+            self::BED_BREAKFAST => 'Bed and Breakfast',
+            self::HIMALAYA => 'Himalaya',
+            self::ALTITUDE => 'Altitude',
+        };
+    }
 
 }
