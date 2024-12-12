b3fc628b7f76bffa1ee7d95eef943f121fec3942
Adesh Bhattarai <adeshbhattarai99@gmail.com>
2024-12-12 18:42:58 +0545

    resources

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -21,7 +21,11 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+
+    protected static ?string $navigationGroup = 'Users';
+    
+
 
     public static function form(Form $form): Form
     {
