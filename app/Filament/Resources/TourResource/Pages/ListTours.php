3aa00c961bed6af0d8e4c61d135a0c1ed5fd982d
Adesh Bhattarai <adeshbhattarai99@gmail.com>
2024-12-26 19:03:44 +0545

    pages

---
+++ app/Filament/Resources/TourResource/Pages/ListTours.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Resources\TourResource\Pages;
+
+use App\Enums\TourType;
+use App\Filament\Resources\TourResource;
+use Filament\Actions;
+use Filament\Resources\Components\Tab;
+use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+
+class ListTours extends ListRecords
+{
+    protected static string $resource = TourResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+{
+    return [
+        'all' => Tab::make('All tours'),
+        'cycling' => Tab::make('Cycling')
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('type', TourType::CYCLING)),
+        'running' => Tab::make('Running')
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('type', TourType::RUNNING)),
+        'sightseeing' => Tab::make('Sight Seeing')
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('type', TourType::SIGHTSEEING)),
+        'photography' => Tab::make('Photography')
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('type', TourType::PHOTOGRAPHY)),
+        'cultural' => Tab::make('Cultural/Meditation')
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('type', TourType::CULTURAL)),
+        'others' => Tab::make('Others')
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('type', TourType::OTHERS)),
+    ];
+}
+}
