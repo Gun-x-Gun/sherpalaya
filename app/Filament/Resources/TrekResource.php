105a0764f97cfae7cf056cb19f9fbf536e6a28a8
Adesh Bhattarai <adeshbhattarai99@gmail.com>
2025-01-16 14:05:16 +0545

    resource

--- app/Filament/Resources/TrekResource.php
+++ app/Filament/Resources/TrekResource.php
@@ -215,7 +215,14 @@ class TrekResource extends Resource
                                             ->columns(7)
                                             ->schema([
                                                 TextInput::make('title')
-                                                    ->columnSpan(7),
+                                                    ->columnSpan(3),
+                                                    Select::make('destinations')
+                                                        ->relationship('destinations', 'name')
+                                                        ->multiple()
+                                                        ->preload()
+                                                        ->searchable()
+                                                        ->columnSpan(4)
+                                                        ->native(false),
                                                 TableRepeater::make('itineraryDetails')
                                                     ->relationship('itineraryDetails')
                                                     ->schema([
