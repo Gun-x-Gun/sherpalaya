510da643529d7f75628aebeccddad93a723c9647
Adesh Bhattarai <adeshbhattarai99@gmail.com>
2025-01-16 16:33:51 +0545

    service

---
+++ app/Filament/Resources/ServiceResource.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ServiceResource\Pages;
+use App\Filament\Resources\ServiceResource\RelationManagers;
+use App\Models\Service;
+use Awcodes\Curator\Components\Forms\CuratorPicker;
+use Filament\Forms;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use RalphJSmit\Filament\Components\Forms\Sidebar;
+
+class ServiceResource extends Resource
+{
+    protected static ?string $model = Service::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationGroup = 'Content';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make('General')
+                    ->columns(2)
+                    ->schema([
+                        TextInput::make('title')
+                            ->columnSpanFull(),
+                        RichEditor::make('description')
+                            ->columnSpanFull()
+                            ->required()
+                            ->toolbarButtons([
+                                // 'attachFiles',
+                                'blockquote',
+                                'bold',
+                                'bulletList',
+                                // 'codeBlock',
+                                'h2',
+                                'h3',
+                                'italic',
+                                'link',
+                                'orderedList',
+                                'redo',
+                                // 'strike',
+                                'underline',
+                                'undo',
+                            ]),
+
+                        CuratorPicker::make('cover_image_id')
+                            ->color('primary')
+                            ->label('Cover Image')
+                            ->hint('for service page')
+                            ->relationship('coverImage', 'id'),
+                        CuratorPicker::make('images')
+                            ->multiple()
+                            ->label('Images')
+                            ->hint('any other relevant images')
+                            ->relationship('images', 'id'),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListServices::route('/'),
+            'create' => Pages\CreateService::route('/create'),
+            'view' => Pages\ViewService::route('/{record}'),
+            'edit' => Pages\EditService::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/Service.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Helpers\CuratorModelHelper;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Service extends Model
+{
+    protected $fillable=[
+        'title',
+        'description',
+        'cover_image_id',
+    ];
+
+    public function coverImage(): BelongsTo
+    {
+        return CuratorModelHelper::belongsTo($this, 'cover_image_id');
+    }
+    public function images(): BelongsToMany
+    {
+        return CuratorModelHelper::belongsToMany($this, 'media_service', 'service_id');
+    }
+}
