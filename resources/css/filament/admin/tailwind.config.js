d6b44881f059f83d31872eebef829c742e42b3ec
Adesh Bhattarai <adeshbhattarai99@gmail.com>
2025-01-18 21:30:48 +0545

    tailwind update

--- resources/css/filament/admin/tailwind.config.js
+++ resources/css/filament/admin/tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         './resources/views/filament/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
         './vendor/awcodes/filament-curator/resources/**/*.blade.php',
+        './vendor/awcodes/overlook/resources/**/*.blade.php',
 
     ],
 }
