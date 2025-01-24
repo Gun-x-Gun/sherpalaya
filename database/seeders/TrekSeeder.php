c4fd9b9e900ffb5ae9d8523eeaa5ae8ac1f71e60
Adesh Bhattarai <adeshbhattarai99@gmail.com>
2025-01-24 16:52:14 +0545

    seeder

--- database/seeders/TrekSeeder.php
+++ database/seeders/TrekSeeder.php
@@ -17,7 +17,7 @@ class TrekSeeder extends Seeder
     public function run(): void
     {
         $trek = Trek::create([
-            'title' => 'Everest Base Camp Trekking',
+            'title' => 'Everest Base Camp Trek',
             'description' => 'Followed by Real Sherpa’s village everybody wants to see a glimpse of the world’s highest mountain and that’s the reason why the Everest Base Camp Trek is very popular. The trek has a number of stunning attractions, not least of these is being able to say you’ve visited the highest mountain in the world. The trek gets you right into the high-altitude heart of the high Himalaya, more so than any other teahouse trek. There are some lovely villages and gompas (monasteries), and the friendly Sherpa people of the Solukhumbu region make trekking through the area a joy.
 
 Everest Base Camp Trekking is known as the one of the most popular trekking in the world with the most thrilling and unforgettable experience of them all. If you are in search of quality service, Nepal Everest base camp trekking company provides you with quality services, guide and other necessary things which are needed for the trekking. We ensure that you won’t regret your decision for choosing us
