a8686caa1dad8791ab94cb199c2c1d926cd4d155
Adesh Bhattarai <adeshbhattarai99@gmail.com>
2025-01-31 19:05:25 +0545

    stat update

--- resources/views/components/stat-widget.blade.php
+++ resources/views/components/stat-widget.blade.php
@@ -3,7 +3,7 @@
     <div class="h-16 ">
 
     </div>
-    <div class="2xl:mx-44 mx-4">
+    {{-- <div class="2xl:mx-44 mx-4">
         <div class="stats  gap-2 w-full">
             <div class="stat">
                 <div class="stat-figure text-base-content size-8">
@@ -34,6 +34,42 @@
                 </div>
                 <div class="stat-title">Tours </div>
                 <div class="stat-value text-success">1000 + </div>
+            </div>
+
+
+        </div>
+    </div> --}}
+    <div class="2xl:mx-44 mx-4">
+        <div class="stats  gap-2 w-full">
+            <div class="stat">
+                <div class="stat-figure text-base-content size-8">
+                    <span class="icon-[mingcute--group-3-line] size-14"></span>
+                </div>
+                <div class="stat-title">Travellers </div>
+                <div class="stat-value text-success">500 +</div>
+            </div>
+            <div class="stat">
+                <div class="stat-figure text-base-content size-8">
+                    <span class="icon-[mingcute--building-6-line] size-14"></span>
+                </div>
+                <div class="stat-title">Associations</div>
+                <div class="stat-value text-success">100 + </div>
+            </div>
+
+            <div class="stat">
+                <div class="stat-figure text-base-content size-8">
+                    <span class="icon-[mingcute--star-line] size-14"></span>
+                </div>
+                <div class="stat-title">Customer Feedback </div>
+                <div class="stat-value text-success">8.9/10 </div>
+            </div>
+
+            <div class="stat">
+                <div class="stat-figure text-base-content size-8">
+                    <span class="icon-[mingcute--certificate-line] size-14"></span>
+                </div>
+                <div class="stat-title">Success Rate </div>
+                <div class="stat-value text-success">90% </div>
             </div>
 
 
