d4af6e3049c5ae9d3243f4732ce0e5ed2914944e
Umanga Pathak <umangapathak999@gmail.com>
2025-01-31 14:58:41 +0545

    Contact us completed

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ContactUs;
 use Illuminate\Http\Request;
 
 class WebsiteController extends Controller
@@ -12,6 +13,28 @@ class WebsiteController extends Controller
 
     public function contactUs(Request $request){
         return view('website.contact_us');
+    }
+
+    public function contactUsSubmit(Request $request){
+        $validatedData = $request->validate([
+            'full_name' => [
+                'required', 'string'
+            ],
+            'email' => [
+                'required', 'string'
+            ],
+            'mobile_number' => [
+                'nullable', 'sometimes'
+            ],
+            'message' => [
+                'required', 'string'
+            ],
+        ]);
+
+        ContactUs::create($validatedData);
+        return view('website.contact_us', [
+            'submitted' => true
+        ]);
     }
 
     public function aboutUs(Request $request){
--- resources/views/website/contact_us.blade.php
+++ resources/views/website/contact_us.blade.php
@@ -17,17 +17,20 @@
                     <div class="h-full pr-6">
                         <p class="mt-3 mb-12 text-lg text-gray-600">
                             Class aptent taciti sociosqu ad
-                            litora torquent per conubia nostra, per inceptos himenaeos. Duis nec ipsum orci. Ut scelerisque
+                            litora torquent per conubia nostra, per inceptos himenaeos. Duis nec ipsum orci. Ut
+                            scelerisque
                             sagittis ante, ac tincidunt sem venenatis ut.
                         </p>
                         <ul class="mb-6 md:mb-0">
                             <li class="flex">
-                                <div class="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round" class="h-6 w-6">
+                                <div
+                                    class="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6">
                                         <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
-                                        <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z">
+                                        <path
+                                            d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z">
                                         </path>
                                     </svg>
                                 </div>
@@ -38,11 +41,13 @@
                                 </div>
                             </li>
                             <li class="flex">
-                                <div class="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round" class="h-6 w-6">
-                                        <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2">
+                                <div
+                                    class="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6">
+                                        <path
+                                            d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2">
                                         </path>
                                         <path d="M15 7a2 2 0 0 1 2 2"></path>
                                         <path d="M15 3a6 6 0 0 1 6 6"></path>
@@ -55,10 +60,11 @@
                                 </div>
                             </li>
                             <li class="flex">
-                                <div class="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round" class="h-6 w-6">
+                                <div
+                                    class="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6">
                                         <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
                                         <path d="M12 7v5l3 3"></path>
                                     </svg>
@@ -72,26 +78,52 @@
                         </ul>
                     </div>
                     <div class="card h-fit max-w-6xl p-5 md:p-12" id="form">
-                        <h2 class="mb-4 text-2xl font-bold">Ready to Get Started?</h2>
-                        <form id="contactForm">
+                        <h2 class="mb-2 text-2xl font-bold">Ready to Get Started?</h2>
+                        @if (isset($submitted))
+                            @if ($submitted === true)
+                                <div class="alert alert-outline alert-success mb-2" role="alert">
+                                    Your message is submitted. Thank you for contacting us!
+                                </div>
+                            @endif
+
+                        @endif
+                        <form id="contactForm" action="/contact" method="POST" class="mt-2">
+                            @csrf
                             <div class="mb-6">
                                 <div class="mx-0 mb-1 sm:mb-4">
                                     <div class="mx-0 mb-1 sm:mb-4">
-                                        <label for="name" class="pb-1 text-xs uppercase tracking-wider"></label>
-                                        <input type="text" id="name" autocomplete="given-name" placeholder="Your name" class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md" name="name">
+                                        <label for="full_name" class="pb-1 text-xs uppercase tracking-wider"></label>
+                                        <input type="text" id="full_name" autocomplete="name"
+                                            placeholder="Your full name"
+                                            class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md"
+                                            name="full_name" required>
                                     </div>
                                     <div class="mx-0 mb-1 sm:mb-4">
                                         <label for="email" class="pb-1 text-xs uppercase tracking-wider"></label>
-                                        <input type="email" id="email" autocomplete="email" placeholder="Your email address" class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md" name="email">
+                                        <input type="email" id="email" autocomplete="email"
+                                            placeholder="Your email address"
+                                            class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md"
+                                            name="email" required>
+                                    </div>
+                                    <div class="mx-0 mb-1 sm:mb-4">
+                                        <label for="mobile_number"
+                                            class="pb-1 text-xs uppercase tracking-wider"></label>
+                                        <input type="tel" id="mobile_number" autocomplete="tel"
+                                            placeholder="Your contact number"
+                                            class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md"
+                                            name="mobile_number">
                                     </div>
                                 </div>
                                 <div class="mx-0 mb-1 sm:mb-4">
                                     <label for="textarea" class="pb-1 text-xs uppercase tracking-wider"></label>
-                                    <textarea id="textarea" name="textarea" cols="30" rows="5" placeholder="Write your message..." class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md"></textarea>
+                                    <textarea id="textarea" name="message" cols="30" rows="5" placeholder="Write your message..."
+                                        class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md" required></textarea>
                                 </div>
                             </div>
                             <div class="text-center">
-                                <button type="submit" class="w-full bg-blue-800 text-white px-6 py-3 font-xl rounded-md sm:mb-0">Send Message</button>
+                                <button type="submit"
+                                    class="w-full bg-blue-800 text-white px-6 py-3 font-xl rounded-md sm:mb-0">Send
+                                    Message</button>
                             </div>
                         </form>
                     </div>
