b77aeb4fac05394776805f5dd3cc428afd740967
Adesh Bhattarai <adeshbhattarai99@gmail.com>
2025-01-31 20:20:15 +0545

    reeview resource

---
+++ app/Filament/Resources/ReviewResource.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ReviewResource\Pages;
+use App\Filament\Resources\ReviewResource\RelationManagers;
+use App\Models\Review;
+use Filament\Forms;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Support\Enums\FontWeight;
+use Filament\Tables;
+use Filament\Tables\Columns\Layout\Panel;
+use Filament\Tables\Columns\Layout\Split;
+use Filament\Tables\Columns\Layout\Stack;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ReviewResource extends Resource
+{
+    protected static ?string $model = Review::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-magnifying-glass-circle';
+
+    protected static ?string $navigationGroup = 'Site';
+
+    protected static ?int $navigationSort = 4;
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make()
+                    ->columns(5)
+                    ->schema([
+                        TextInput::make('name')
+                            ->columnSpan(4)
+                            ->required(),
+                        Toggle::make('display_in_home_page')
+                            ->inline(false)
+                                ->default(false)
+                                ->columnSpan(1),
+                        Textarea::make('title')
+                            ->rows(1)
+                            ->columnSpanFull()
+                            ->required(),
+                        RichEditor::make('description')
+                            ->required()
+                            ->columnSpanFull()
+                            ->toolbarButtons([
+                                // 'attachFiles',
+                                'blockquote',
+                                'bold',
+                                'bulletList',
+                                // 'codeBlock',
+                                'h2',
+                                'h3',
+                                'italic',
+                                'link',
+                                'orderedList',
+                                'redo',
+                                // 'strike',
+                                'underline',
+                                'undo',
+                            ]),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Split::make([
+                    TextColumn::make('name')
+                        ->size(TextColumn\TextColumnSize::Large)
+                        ->weight(FontWeight::Bold),
+                    TextColumn::make('title'),
+                ])->from('md'),
+                Panel::make([
+                    Stack::make([
+                        TextColumn::make('description')
+                            ->html()
+                            ->words(25),
+                    ]),
+                ])->collapsible()
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListReviews::route('/'),
+            'create' => Pages\CreateReview::route('/create'),
+            'view' => Pages\ViewReview::route('/{record}'),
+            'edit' => Pages\EditReview::route('/{record}/edit'),
+        ];
+    }
+}
