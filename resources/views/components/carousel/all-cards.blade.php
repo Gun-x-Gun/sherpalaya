5517ea9e53f9050d3f6c838dde2415ab721a0200
Umanga Pathak <umangapathak999@gmail.com>
2025-01-31 19:45:57 +0545

    backdrop stat added

--- resources/views/components/carousel/all-cards.blade.php
+++ resources/views/components/carousel/all-cards.blade.php
@@ -1,138 +1,17 @@
-{{-- <div class="max-w-full bg-slate-300 ">
-    <div class="h-16">
-    </div>
-    <div class="card-header font-extra-bold capitalize tracking-normal  text-center underline underline-offset-2 decoration-dashed w-full">
-        <h5 class="card-title text-4xl  text-blue-800">
-            Our Services </h5>
-    </div>
-    <div class="h-12">
-
-    </div>
-    <div class="lg:grid lg:grid-cols-2 gap-0 flex flex-col lg:max-[77%] 2xl:mx-44 md:grid md:grid-cols-1 mx-4 mb-4">
-        <!-- Card 1 -->
-        <div class="card card-side group hover:shadow border border-gray-200 rounded-none overflow-hidden border-none shadow-none bg-slate-400/20 lg:grid lg:grid-cols-2 md:grid md:grid-cols-2 flex flex-col "
-            dir="ltr">
-            <figure class="md:w-full lg:w-full w-full">
-                <img src="{{ asset('photos/62017703_2502887636410934_2901252517276942336_n.jpg') }}" alt="Shoes"
-                    class="transition-transform duration-500 group-hover:scale-110 h-full w-full object-cover" />
-            </figure>
-            <div class="card-body md:w-full lg:w-full w-full p-2 flex flex-col ">
-                <h5 class="card-title text-white mb-2.5">Treks</h5>
-                <p class="mb-6 text-secondary">Nike Air Max is a popular line of athletic shoes that feature Nike's
-                    signature
-                    Air
-                    cushioning technology in the sole.</p>
-                <a href="/tours">
-                    <div class="card-actions flex gap-2 flex-row items-center">
-                        <span class="text-white font-medium">Explore</span>
-                        <span class="icon-[material-symbols--arrow-right-alt-rounded] link link-primary"
-                            style="width: 32px; height: 32px;">
-                        </span>
-                    </div>
-                </a>
-            </div>
-        </div>
-        <!-- Card 1 -->
-        <div class="card card-side group hover:shadow border border-gray-200 rounded-none overflow-hidden border-none shadow-none bg-slate-400/20 lg:grid lg:grid-cols-2 md:grid md:grid-cols-2 flex flex-col "
-            dir="ltr">
-            <figure class="md:w-full lg:w-full w-full">
-                <img src="{{ asset('photos/62017703_2502887636410934_2901252517276942336_n.jpg') }}" alt="Shoes"
-                    class="transition-transform duration-500 group-hover:scale-110 h-full w-full object-cover" />
-            </figure>
-            <div class="card-body md:w-full lg:w-full w-full p-2 flex flex-col ">
-                <h5 class="card-title text-white mb-2.5">Expeditions</h5>
-                <p class="mb-6 text-secondary">Nike Air Max is a popular line of athletic shoes that feature Nike's
-                    signature
-                    Air
-                    cushioning technology in the sole.</p>
-                <a href="/tours">
-                    <div class="card-actions flex gap-2 flex-row items-center">
-                        <span class="text-white font-medium">Explore</span>
-                        <span class="icon-[material-symbols--arrow-right-alt-rounded] link link-primary"
-                            style="width: 32px; height: 32px;">
-                        </span>
-                    </div>
-                </a>
-            </div>
-        </div>
-        <!-- Card 1 -->
-        <div class="card card-side group hover:shadow border border-gray-200 rounded-none overflow-hidden border-none shadow-none bg-slate-400/20 lg:grid lg:grid-cols-2 md:grid md:grid-cols-2 flex flex-col "
-            dir="rtl">
-            <figure class="md:w-full lg:w-full w-full">
-                <img src="{{ asset('photos/62017703_2502887636410934_2901252517276942336_n.jpg') }}" alt="Shoes"
-                    class="transition-transform duration-500 group-hover:scale-110 h-full w-full object-cover" />
-            </figure>
-            <div class="card-body md:w-full lg:w-full w-full p-2 flex flex-col ">
-                <h5 class="card-title text-white mb-2.5">Peaks</h5>
-                <p class="mb-6 text-secondary">Nike Air Max is a popular line of athletic shoes that feature Nike's
-                    signature
-                    Air
-                    cushioning technology in the sole.</p>
-                <a href="/tours">
-                    <div class="card-actions flex gap-2 flex-row items-center">
-                        <span class="text-white font-medium">Explore</span>
-                        <span class="icon-[material-symbols--arrow-right-alt-rounded] link link-primary"
-                            style="width: 32px; height: 32px;">
-                        </span>
-                    </div>
-                </a>
-            </div>
-        </div>
-        <!-- Card 1 -->
-        <div class="card card-side group hover:shadow border border-gray-200 rounded-none overflow-hidden border-none shadow-none bg-slate-400/20 lg:grid lg:grid-cols-2 md:grid md:grid-cols-2 flex flex-col "
-            dir="rtl">
-            <figure class="md:w-full lg:w-full w-full">
-                <img src="{{ asset('photos/62017703_2502887636410934_2901252517276942336_n.jpg') }}" alt="Shoes"
-                    class="transition-transform duration-500 group-hover:scale-110 h-full w-full object-cover" />
-            </figure>
-            <div class="card-body md:w-full lg:w-full w-full p-2 flex flex-col ">
-                <h5 class="card-title text-white mb-2.5">Tours</h5>
-                <p class="mb-6 text-secondary">Nike Air Max is a popular line of athletic shoes that feature Nike's
-                    signature
-                    Air
-                    cushioning technology in the sole.</p>
-                <a href="/tours">
-                    <div class="card-actions flex gap-2 flex-row items-center">
-                        <span class="text-white font-medium">Explore</span>
-                        <span class="icon-[material-symbols--arrow-right-alt-rounded] link link-primary"
-                            style="width: 32px; height: 32px;">
-                        </span>
-                    </div>
-                </a>
-            </div>
-        </div>
-    </div>
-    <div class="h-24">
-
-    </div>
-</div> --}}
-
-
-
-
-{{-- <span class="icon-[weui--arrow-filled]" style="width: 24px; height: 48px; color: #6a4ca4;"></span> --}}
 <div class="bg-blue-100/50">
     <div class="h-8  "></div>
     <div class="2xl:mx-44 mx-4 ">
-        {{-- <h5 class="text-3xl  font-extralight text-left line-clamp-2 tracking-wider text-secondary"> Sherpalaya
-        </h5> --}}
         <h3 class="text-4xl tracking-normal text-black uppercase text-center">Activities ...</h3>
-        {{-- <p
-            class="text-md mt-2 text-preety text-balance text-slate-500 md:text-wrap md:text-justify first-line:uppercase first-line:tracking-widest first-line:font-light ">
-            For those seeking the ultimate challenge, Sherpalaya’s expedition services offer unparalleled support and
-            expertise. Whether it’s climbing Everest or venturing to the lesser-known peaks, Sherpalaya handles every
-            detail, from permits to logistics. With our experienced Sherpa team by your side, we transform daunting
-            Featured into achievable milestones, ensuring a fulfilling and transformative adventure. </p> --}}
     </div>
 </div>
 <div class="bg-blue-100/50 max-w-full">
     <div class="h-8"></div>
 
     <div class="md:grid grid-cols-3 gap-4 2xl:mx-44 mx-4 flex flex-col">
-        <a href="/expeditions" class=" col-span-2">
+        <a href="/expeditions" class="col-span-2">
             <div
-                class="card rounded-md image-full h-96 w-full relative flex items-end  card-side group hover:shadow border">
-                <figure class="h-full w-full ">
+                class="card rounded-md image-full h-96 w-full relative flex items-end  card-side group hover:shadow border expedition-activity-card">
+                <figure class="h-full w-full">
                     <img src="{{ asset('photos/mountain1.jpg') }}" alt="overlay image"
                         class="transition-transform duration-500 group-hover:scale-110 h-full w-full object-cover brightness-75" />
                 </figure>
@@ -141,6 +20,17 @@
                         <h2 class="font-extrabold text-white text-4xl uppercase">
                             Expeditions
                         </h2>
+                    </div>
+                </div>
+                <div class="absolute bottom-0 opacity-0 group-hover:opacity-100 w-min">
+                    <div class="flex flex-row">
+                        <div class="flex-none">
+                            <span class="icon-[hugeicons--angle] size-44 rotate-12 text-black opacity-35"></span>
+
+                        </div>
+                        <div class="flex-none text-yellow-200 font-extrabold text-[6rem] opacity-60">
+                            50+
+                        </div>
                     </div>
                 </div>
             </div>
@@ -159,6 +49,18 @@
                         <h2 class="font-bold text-white text-4xl ">
                             Treks
                         </h2>
+                    </div>
+                </div>
+                <div class="absolute bottom-0 opacity-0 group-hover:opacity-100 w-min">
+                    <div class="flex flex-row">
+                        <div class="flex-none">
+                            <span
+                                class="icon-[mingcute--mountain-2-line] size-44 rotate-12 text-black opacity-35"></span>
+
+                        </div>
+                        <div class="flex-none text-yellow-200 font-extrabold text-[6rem] opacity-60">
+                            200+
+                        </div>
                     </div>
                 </div>
             </div>
@@ -183,6 +85,17 @@
                         </h2>
                     </div>
                 </div>
+                <div class="absolute bottom-0 opacity-0 group-hover:opacity-100 w-min">
+                    <div class="flex flex-row">
+                        <div class="flex-none">
+                            <span class="icon-[hugeicons--bus-01] size-44 rotate-12 text-black opacity-35"></span>
+
+                        </div>
+                        <div class="flex-none text-yellow-200 font-extrabold text-[6rem] opacity-60">
+                            1000+
+                        </div>
+                    </div>
+                </div>
             </div>
         </a>
 
@@ -199,6 +112,17 @@
                         <h2 class="font-bold text-white  text-4xl ">
                             Peaks
                         </h2>
+                    </div>
+                </div>
+                <div class="absolute bottom-0 opacity-0 group-hover:opacity-100 w-min">
+                    <div class="flex flex-row">
+                        <div class="flex-none">
+                            <span class="icon-[hugeicons--gem] size-44 rotate-12 text-black opacity-35"></span>
+
+                        </div>
+                        <div class="flex-none text-yellow-200 font-extrabold text-[6rem] opacity-60">
+                            100+
+                        </div>
                     </div>
                 </div>
             </div>
