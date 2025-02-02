8263a303d7d32f3a104a818eb87445ecbedfc98e
Umanga Pathak <umangapathak999@gmail.com>
2025-02-02 20:35:11 +0545

    fix duplicate

--- resources/views/website/company/our_services.blade.php
+++ resources/views/website/company/our_services.blade.php
@@ -21,22 +21,8 @@
             </div>
         </div>
         <div class="h-8 "></div>
-        <div class="2xl:mx-44 mx-4 text-left">
-            <h1
-                class="text-2xl lg:text-5xl  font-light  line-clamp-2 tracking-wider text-primary uppercase wrap text-pretty">
-                Our Services In Nepal
-            </h1>
-            {{-- <h3 class="text-3xl tracking-widest text-primary "> With Sherpalaya</h3> --}}
-            <p
-                class="text-md text-left  mt-2  text-blue-600 first-line:uppercase first-line:tracking-widest first-line:font-light ">
-                For those seeking the ultimate challenge, Sherpalaya’s expedition services offer unparalleled support
-                and
-                expertise. Whether it’s climbing Everest or venturing to the lesser-known peaks, Sherpalaya handles
-                every
-                detail, from permits to logistics. With our experienced Sherpa team by your side, we transform daunting
-                service into achievable milestones, ensuring a fulfilling and transformative adventure. </p>
-        </div>
-        <div class="h-12 "></div>
+
+        {{-- <div class="h-12 "></div> --}}
 
 
 
