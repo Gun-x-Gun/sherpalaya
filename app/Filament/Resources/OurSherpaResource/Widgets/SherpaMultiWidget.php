e5b85110f36f9ab34956e1809e40aef15deac3d5
Adesh Bhattarai <adeshbhattarai99@gmail.com>
2025-02-04 16:38:54 +0545

    widgets

---
+++ app/Filament/Resources/OurSherpaResource/Widgets/SherpaMultiWidget.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\OurSherpaResource\Widgets;
+
+use Filament\Widgets\Widget;
+use Kenepa\MultiWidget\MultiWidget;
+
+class SherpaMultiWidget extends MultiWidget
+{
+    public array $widgets = [
+        SherpaExpeditionsTableWidget::class,
+        SherpaPeaksTableWidget::class,
+        SherpaTreksTableWidget::class,
+        SherpaToursTableWidget::class,
+    ];
+}
