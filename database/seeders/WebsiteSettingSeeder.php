5b77b88beec7da3888e0dff4915d519983ee4c29
Adesh Bhattarai <adeshbhattarai99@gmail.com>
2025-02-04 19:03:25 +0545

    seeder

--- database/seeders/WebsiteSettingSeeder.php
+++ database/seeders/WebsiteSettingSeeder.php
@@ -62,7 +62,7 @@ class WebsiteSettingSeeder extends Seeder
         $pageSetting->tour_page_cover_image_id = CuratorSeederHelper::resolveFileData(public_path('/photos/culture2.jpg'))->id;
         $pageSetting->peak_page_cover_image_id = CuratorSeederHelper::resolveFileData(public_path('/photos/lobuche.jpg'))->id;
         $pageSetting->service_page_cover_image_id = CuratorSeederHelper::resolveFileData(public_path('/photos/temple.jpg'))->id;
-        $pageSetting->about_us_page_cover_image_id = CuratorSeederHelper::resolveFileData(public_path('/photos/temple.jpg'))->id;
+        $pageSetting->about_us_page_cover_image_id = CuratorSeederHelper::resolveFileData(public_path('/photos/qualitymount1.png'))->id;
 
         $pageSetting->expedition_page_content = "Push your limits and embark on an extraordinary Himalayan expedition. Our expertly guided journeys take you to some of the highest and most challenging peaks on the planet. Experience raw adventure, breathtaking landscapes, and the triumph of reaching new heights with our experienced mountaineers and support teams.";
         $pageSetting->trek_page_content = "Step into the heart of the Himalayas with our unforgettable trekking adventures. Walk through lush forests, rugged mountain trails, and culturally rich villages as you discover Nepal’s most iconic routes. Whether you seek a short scenic trek or an epic multi-day journey, we create experiences that inspire.";
