4f3c3b36926b90ed17dfdf02e4c50454ebe0f335
Umanga Pathak <umangapathak999@gmail.com>
2025-02-05 20:03:49 +0545

    added category seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         CuratorSeederHelper::clearStorage();
 
         $this->call([
+            CategorySeeder::class,
             // KeyPointSeeder::class,
             ShieldSeeder::class,
             UserSeeder::class,
