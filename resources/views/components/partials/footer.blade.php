e7ce1f427eea3671242caebfac6b62fab859b44f
Subeen928 <subintmg321@gmail.com>
2025-02-07 20:48:22 +0545

    footer update

--- resources/views/components/partials/footer.blade.php
+++ resources/views/components/partials/footer.blade.php
@@ -1,33 +1,99 @@
-   
-   <div style="background-image: url('{{ asset('/photos/background1.jpg') }}');" class="bg-cover bg-center w-full  object-top h-60">
-   
-   {{-- <div class="bg-black"> --}}
-    <footer class="footer footer-center text-white rounded-none p-6 opacity-100 sticky bottom-0 " >
-          <nav class="grid grid-flow-col gap-4 text-white font-bold uppercase">
-              <a href="/about_us" class="link link-hover  ">About</a>
-              <a href="/contact" class="link link-hover">Contact</a>
-              <a href="/services" class="link link-hover">Services</a>
-              <a href="" class="link link-hover">Policy</a>
-          </nav>
-          <nav>
-              <div class="flex gap-4">
-                  <a href="#" class="link link-animated" aria-label="Facebook Link">
-                      <span class="icon-[tabler--brand-facebook] size-6"></span>
-                  </a>
-                  <a href="#" class="link link-animated" aria-label="X Link">
-                      <span class="icon-[tabler--brand-x] size-6"></span>
-                  </a>
-                  <a href="#" class="link link-animated" aria-label="Linkedin Link">
-                      <span class="icon-[tabler--brand-linkedin] size-6"></span>
-                  </a>
-              </div>
-          </nav>
-          
-          <aside>
-              <p>Copyright © 2024 - All right reserved by Sherpalaya</p>
-          </aside>
-      </footer>
-  </div>
-  
+<div style="background-image: url('{{ asset('/photos/background1.jpg') }}');"
+    class="bg-cover bg-center w-full  object-top">
+    <div class="backdrop-blur-md">
+        <div class="mx-4 2xl:mx-44">
+            <div class="h-10"></div>
+            <footer class="footer">
+                <div class="gap-6">
+                    {{-- <div class="flex items-end gap-2 text-xl uppercase font-bold text-white">
+                        <a class="link text-base-content link-neutral  font-semibold no-underline" href="/home">
+                            <img src="{{ asset('photos/logo.png') }}" alt="Sherpalaya Logo" class="h-8 w-12">
+                        </a>
+                        <span>Sherpalaya</span>
+                    </div> --}}
+                    <div class="flex items-center gap-4 text-xl uppercase font-semibold text-blue-100  text-wrap">
+                        <img src="{{ asset('photos/logo.png') }}" alt="Sherpalaya Logo" class="h-6 w-6">
+                        <span>Sherpalaya pvt. ltd.</span>
+                    </div>
 
-  {{-- src="{{ asset('photos/DSCF4385.JPG') }}" --}}
+                    <div class="flex items-center gap-4 text-base capitalize font-normal text-blue-100 text-wrap ">
+                        <span class="icon-[iconoir--map-pin] size-6 text-blue-100"></span>
+                        <span>Kathmandu, thamel, street no 6</span>
+                    </div>
+                    <a class="link text-base-content link-neutral texl-lg font-semibold no-underline"
+                        href="mailto:sherpalaya@gmail.com">
+                        <div
+                            class="flex items-center gap-4 text-lg lowercase font-normal text-blue-100 hover:underline">
+                            <span class="icon-[iconoir--mail] size-6"></span>
+                            <span>Sherpalaya@gmail.com</span>
+                        </div>
+                    </a>
+                    <div class="flex items-center gap-4 text-lg uppercase font-normal text-blue-100">
+                        <span class="icon-[iconoir--phone-income] size-6"></span>
+                        <span>9800112233</span>
+                    </div>
+                    {{-- <a class="link text-base-content link-neutral texl-lg font-semibold no-underline"> --}}
+
+
+
+                </div>
+                <nav class="text-base-content capitalize">
+                    <h6 class="footer-title font-bold text-blue-50 uppercase">Activities</h6>
+                    <a href="#" class="link link-hover text-blue-100 capitalize">Expeditions</a>
+                    <a href="#" class="link link-hover text-blue-100">Peaks</a>
+                    <a href="#" class="link link-hover text-blue-100">treks</a>
+                    <a href="#" class="link link-hover text-blue-100">Tours</a>
+                </nav>
+                <nav class="text-base-content capitalize">
+                    <h6 class="footer-title font-bold text-blue-50 uppercase">Company</h6>
+                    <a href="#" class="link link-hover text-blue-100">About</a>
+                    <a href="#" class="link link-hover text-blue-100">Our Sherpas</a>
+                    <a href="#" class="link link-hover text-blue-100">Services</a>
+
+                </nav>
+                <nav class="text-base-content">
+                    <h6 class="footer-title font-bold text-blue-50 uppercase">Legal</h6>
+                    <a href="#" class="link link-hover text-blue-100">Terms of use</a>
+                    <a href="#" class="link link-hover text-blue-100">Privacy policy</a>
+                    <a href="#" class="link link-hover text-blue-100">Cookie policy</a>
+                </nav>
+                <nav class="text-base-content">
+                    <div class="flex items-center gap- text-base uppercase font-semibold text-blue-200  text-wrap">
+                        <span>Follow us</span>
+                    </div>
+                    <div class="flex gap-4 text-blue-300">
+                        <a href="#" class="link link-animated" aria-label="Facebook Link">
+                            <span class="icon-[tabler--brand-facebook] size-6"></span>
+                        </a>
+                        <a href="#" class="link link-animated" aria-label="X Link">
+                            <span class="icon-[tabler--brand-x] size-6"></span>
+                        </a>
+                        <a href="#" class="link link-animated" aria-label="Linkedin Link">
+                            <span class="icon-[tabler--brand-linkedin] size-6"></span>
+                        </a>
+                    </div>
+                    {{-- <div class="gap-4 hover:underline"> --}}
+                    <a href="https://wa.me/{{ config('services.whatsapp.number') }}">
+                        <div class="flex items-center gap-2 text-base uppercase font-semibold text-green-300  text-wrap">
+                            <span>Reach Us Via</span>
+                        </div>
+                        <div class="flex items-center gap-2 text-base Capitalize font-normal text-green-300 mt-2">
+                            <span class="icon-[tabler--brand-whatsapp] size-7"></span>
+                            WhatsApp
+                        </div>
+                    </a>
+                    {{-- </div> --}}
+                </nav>
+            </footer>
+            <footer class="footer footer-center bg-transparent rounded ">
+                <div class="h-2"></div>
+                <aside class="text-blue-300 xl:mb-8">
+                    <p>Copyright © 2025 - All right reserved by Sherpalaya</p>
+                </aside>
+                <div class="h-10 xl:hidden"></div>
+
+            </footer>
+        </div>
+
+    </div>
+</div>
