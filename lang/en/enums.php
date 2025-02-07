85918e0e4161cce1747f8aaee5214c1a25c38749
Adesh Bhattarai <adeshbhattarai99@gmail.com>
2025-02-07 19:15:50 +0545

    enums added

--- lang/en/enums.php
+++ lang/en/enums.php
@@ -2,8 +2,34 @@
 
 return [
     'category-type' => [
-        'trek' => 'Trek En',
-        'expedition' => 'Expedition En',
-        'peak' => 'Peak En',
-    ]
+        'trek' => 'Trek',
+        'expedition' => 'Expedition',
+        'peak' => 'Peak',
+    ],
+    'trek-difficulty' => [
+    'easy' => 'Easy',
+    'moderate' => 'Moderate',
+    'hard' => 'Hard',
+    'challenging' => 'Challenging',
+    ],
+    'itinerary-types' => [
+        'flight' => 'Flight',
+        'drive' => 'Drive',
+        'trek' => 'Trek',
+        'trek-hours' => 'Trek Hours',
+        'rest' => 'Rest',
+        'accomodation' => 'Accomodation',
+        'himalaya' => 'Himalaya',
+        'altitude' => 'Altitude',
+        'helicopter' => 'Helicopter',
+        'others' => 'Others',
+    ],
+    'tour-type' => [ // Or 'itinerary-types' if these are part of itineraries
+    'sightseeing' => 'Sightseeing', // or 'Tourisme' (tourism)
+    'cycling' => 'Cycling', // or 'Vélo' (bicycle)
+    'running' => 'Running', // or 'Course' (running)
+    'photography' => 'Photography',
+    'cultural' => 'Cultural', // or 'Culture' (culture)
+    'others' => 'Others', // or 'Divers'
+    ],
 ];
--- lang/fr/enums.php
+++ lang/fr/enums.php
@@ -2,8 +2,38 @@
 
 return [
     'category-type' => [
-        'trek' => 'Trek Fr',
-        'expedition' => 'Expedition Fr',
-        'peak' => 'Peak Fr',
-    ]
+        'trek' => 'Randonnée',
+        'expedition' => 'Expédition',
+        'peak' => 'Sommet',
+    ],
+    'trek-difficulty' => [
+        'easy' => 'Facile',
+        'moderate' => 'Modéré',
+        'hard' => 'Difficile',
+        'challenging' => 'Corsé',
+    ],
+    'inquiry-types' => [
+        'booking' => 'Réserver',
+        'inquiry' => 'Renseignez-vous maintenant',
+    ],
+    'itinerary-types' => [
+        'flight' => 'Vol',
+        'drive' => 'Trajet en voiture',
+        'trek' => 'Trek',
+        'trek-hours' => 'Heures de marche',
+        'rest' => 'Repos',
+        'accomodation' => 'Hébergement',
+        'himalaya' => 'Himalaya',
+        'altitude' => 'Altitude',
+        'helicopter' => 'Hélicoptère',
+        'others' => 'Autres',
+    ],
+    'tour-type' => [ // Or 'itinerary-types' if these are part of itineraries
+        'sightseeing' => 'Visites touristiques', // or 'Tourisme' (tourism)
+        'cycling' => 'Cyclisme', // or 'Vélo' (bicycle)
+        'running' => 'Course à pied', // or 'Course' (running)
+        'photography' => 'Photographie',
+        'cultural' => 'Culturel', // or 'Culture' (culture)
+        'others' => 'Autres', // or 'Divers'
+    ],
 ];
