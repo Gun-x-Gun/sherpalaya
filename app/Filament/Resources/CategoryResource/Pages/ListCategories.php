e9400326cf9ad6b2158acaeceec4335131508a2c
brownieandsnickers <iamdivya1009@gmail.com>
2025-02-11 19:02:18 +0545

    category tab

--- app/Filament/Resources/CategoryResource/Pages/ListCategories.php
+++ app/Filament/Resources/CategoryResource/Pages/ListCategories.php
@@ -2,10 +2,13 @@
 
 namespace App\Filament\Resources\CategoryResource\Pages;
 
+use App\Enums\CategoryTypes;
 use App\Filament\Resources\CategoryResource;
 use App\Traits\Filament\TranslatableListRecords;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListCategories extends ListRecords
 {
@@ -19,4 +22,17 @@ class ListCategories extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
+
+    public function getTabs(): array
+{
+    return [
+        'all' => Tab::make('All'),
+        'expedition' => Tab::make('Expedition')
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('type', CategoryTypes::EXPEDITION)),
+        'trek' => Tab::make('Trek')
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('type', CategoryTypes::TREK)),
+        'tour' => Tab::make('Tour')
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('type', CategoryTypes::TOUR)),
+    ];
+}
 }
