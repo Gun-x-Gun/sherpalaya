e8db56cf48ed51cc6d9ddf464c4c1b894e1941b9
Adesh Bhattarai <adeshbhattarai99@gmail.com>
2025-03-04 15:41:25 +0545

    pages

--- resources/views/website/company/our_team.blade.php
+++ resources/views/website/company/our_team.blade.php
@@ -2,7 +2,7 @@
     <div class="bg-blue-100/10 font-body">
         <div class="card--rounded-none image-full  bg-blue-100/50 h-[60vh]">
             <figure class="h-[60vh] w-full">
-                <img src="{{ asset('/photos/mountain2.jpg') }}" alt="Trekking background image"
+                <img src="{{ asset('/photos/activity2.JPG') }}" alt="Trekking background image"
                     class="h-[60vh] w-full object-cover brightness-50" />
             </figure>
             <div class="card-body">
