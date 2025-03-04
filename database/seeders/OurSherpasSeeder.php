2ee21481dc98507be5d44cf099208ccbf79934e4
Adesh Bhattarai <adeshbhattarai99@gmail.com>
2025-03-04 18:14:04 +0545

    sherpa update

--- database/seeders/OurSherpasSeeder.php
+++ database/seeders/OurSherpasSeeder.php
@@ -15,73 +15,84 @@ class OurSherpasSeeder extends Seeder
     public function run(): void
     {
         // Fetch related models
-        $expeditions = Expedition::limit(2)->get();
-        $treks = Trek::limit(2)->get();
-        $tours = Tour::limit(1)->get();
+        $expeditions = Expedition::limit(3)->get();
 
         // Ensure related models exist
-        if ($expeditions->count() < 2 ||  $treks->count() < 2 || $tours->count() < 1) {
+        if ($expeditions->count() < 3) {
             return;
         }
 
-        // Define two sherpas manually
-        $sherpas = [
-            [
+        $sherpa1 = [
                 'name' => 'Tenzing Norgay',
                 'title' => 'Legendary Mountaineer',
                 'description' => [
                     'en' => 'One of the first climbers to reach the summit of Mount Everest.',
                     'fr' => 'L\'un des premiers alpinistes à atteindre le sommet du mont Everest.',
                 ],
-            ],
-            [
+        ];
+        $sherpa = OurSherpa::create($sherpa1);
+
+        CuratorSeederHelper::seedBelongsTo(
+            $sherpa,
+            'profile_picture_id',
+            public_path('photos/oursherpa1.jpg')
+        );
+        CuratorSeederHelper::seedBelongsToMany(
+            $sherpa,
+            'awardsAndCertificates',
+            public_path('photos/culture.jpg')
+        );
+        CuratorSeederHelper::seedBelongsToMany(
+            $sherpa,
+            'awardsAndCertificates',
+            public_path('photos/culture2.jpg')
+        );
+        CuratorSeederHelper::seedBelongsToMany(
+            $sherpa,
+            'awardsAndCertificates',
+            public_path('photos/culture3.jpg')
+        );
+
+
+        foreach ($expeditions as $index => $expedition) {
+            $sherpa->expeditions()->attach($expedition->id, ['order' => $index + 1]);
+        }
+        // Define two sherpas manually
+        $oursherpa2 = [
                 'name' => 'Kami Rita Sherpa',
                 'title' => 'Everest Record Holder',
                 'description' => [
                     'en' => 'Holds the record for the most Everest summits.',
                     'fr' => 'Détient le record du plus grand nombre de sommets de l\'Everest.',
                 ],
-            ],
         ];
+        $sherpa = OurSherpa::create($oursherpa2);
+
+        CuratorSeederHelper::seedBelongsTo(
+            $sherpa,
+            'profile_picture_id',
+            public_path('photos/oursherpa2.jpeg')
+        );
+        CuratorSeederHelper::seedBelongsToMany(
+            $sherpa,
+            'awardsAndCertificates',
+            public_path('photos/culture.jpg')
+        );
+        CuratorSeederHelper::seedBelongsToMany(
+            $sherpa,
+            'awardsAndCertificates',
+            public_path('photos/culture2.jpg')
+        );
+        CuratorSeederHelper::seedBelongsToMany(
+            $sherpa,
+            'awardsAndCertificates',
+            public_path('photos/culture3.jpg')
+        );
 
 
-        foreach ($sherpas as $sherpaData) {
-            $sherpa = OurSherpa::create($sherpaData);
-
-            CuratorSeederHelper::seedBelongsTo(
-                $sherpa,
-                'profile_picture_id',
-                public_path('photos/Chor.jpg')
-            );
-            CuratorSeederHelper::seedBelongsToMany(
-                $sherpa,
-                'awardsAndCertificates',
-                public_path('photos/culture.jpg')
-            );
-            CuratorSeederHelper::seedBelongsToMany(
-                $sherpa,
-                'awardsAndCertificates',
-                public_path('photos/culture2.jpg')
-            );
-            CuratorSeederHelper::seedBelongsToMany(
-                $sherpa,
-                'awardsAndCertificates',
-                public_path('photos/culture3.jpg')
-            );
-
-
-            foreach ($expeditions as $index => $expedition) {
-                $sherpa->expeditions()->attach($expedition->id, ['order' => $index + 1]);
-            }
-            // Attach Two Treks
-            foreach ($treks as $index => $trek) {
-                $sherpa->treks()->attach($trek->id, ['order' => $index + 1]);
-            }
-
-            // Attach Four Tours
-            foreach ($tours as $index => $tour) {
-                $sherpa->tours()->attach($tour->id, ['order' => $index + 1]);
-            }
+        foreach ($expeditions as $index => $expedition) {
+            $sherpa->expeditions()->attach($expedition->id, ['order' => $index + 1]);
         }
+        
     }
 }
