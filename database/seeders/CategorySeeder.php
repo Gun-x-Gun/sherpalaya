f9e0c752feb79575c80ecccebeff1238fe81a5ed
Adesh Bhattarai <adeshbhattarai99@gmail.com>
2025-03-18 19:37:57 +0545

    updated french

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -19,42 +19,42 @@ class CategorySeeder extends Seeder
         Category::create([
             'name' => [
                 'en' => '8000+',
-                'fr' => '8000+ (fr)',
+                'fr' => '8000+',
             ],
             'type' => CategoryTypes::EXPEDITION
         ]);
         Category::create([
             'name' => [
                 'en' => '7000+',
-                'fr' => '7000+ (fr)',
+                'fr' => '7000+',
             ],
             'type' => CategoryTypes::EXPEDITION
         ]);
         Category::create([
             'name' => [
                 'en' => '6000+',
-                'fr' => '6000+ (fr)',
+                'fr' => '6000+',
             ],
             'type' => CategoryTypes::EXPEDITION
         ]);
         Category::create([
             'name' => [
                 'en' => 'Luxury',
-                'fr' => 'Luxury (fr)',
+                'fr' => 'Luxury',
             ],
             'type' => CategoryTypes::EXPEDITION
         ]);
         Category::create([
             'name' => [
                 'en' => 'Seven Summits',
-                'fr' => 'Seven Summits (fr)',
+                'fr' => 'Seven Summits',
             ],
             'type' => CategoryTypes::EXPEDITION
         ]);
         Category::create([
             'name' => [
                 'en' => 'Others',
-                'fr' => 'Others (fr)',
+                'fr' => 'Autres',
             ],
             'type' => CategoryTypes::EXPEDITION
         ]);
@@ -64,35 +64,28 @@ class CategorySeeder extends Seeder
         Category::create([
             'name' => [
                 'en' => 'Everest',
-                'fr' => 'Everest (fr)',
+                'fr' => 'Everest',
             ],
             'type' => CategoryTypes::TREK
         ]);
         Category::create([
             'name' => [
                 'en' => 'Annapurna',
-                'fr' => 'Annapurna (fr)',
+                'fr' => 'Annapurna',
             ],
             'type' => CategoryTypes::TREK
         ]);
         Category::create([
             'name' => [
                 'en' => 'Langtang',
-                'fr' => 'Langtang (fr)',
+                'fr' => 'Langtang',
             ],
             'type' => CategoryTypes::TREK
         ]);
-        // Category::create([
-        //     'name' => [
-        //         'en' => 'Kangchanjunga',
-        //         'fr' => 'Kangchanjunga (fr)',
-        //     ],
-        //     'type' => CategoryTypes::TREK
-        // ]);
         Category::create([
             'name' => [
                 'en' => 'Other',
-                'fr' => 'Other (fr)',
+                'fr' => 'Autres',
             ],
             'type' => CategoryTypes::TREK
         ]);
@@ -102,42 +95,42 @@ class CategorySeeder extends Seeder
         Category::create([
             'name' => [
                 'en' => 'Sightseeing',
-                'fr' => 'Sightseeing (fr)',
+                'fr' => 'Tourisme',
             ],
             'type' => CategoryTypes::TOUR
         ]);
         Category::create([
             'name' => [
                 'en' => 'Cycling',
-                'fr' => 'Cycling (fr)',
+                'fr' => 'Cyclisme',
             ],
             'type' => CategoryTypes::TOUR
         ]);
         Category::create([
             'name' => [
                 'en' => 'Running',
-                'fr' => 'Running (fr)',
+                'fr' => 'Course à pied',
             ],
             'type' => CategoryTypes::TOUR
         ]);
         Category::create([
             'name' => [
                 'en' => 'Photography',
-                'fr' => 'Photography (fr)',
+                'fr' => 'Photographie',
             ],
             'type' => CategoryTypes::TOUR
         ]);
         Category::create([
             'name' => [
                 'en' => 'Cultural/Meditation',
-                'fr' => 'Cultural/Meditation (fr)',
+                'fr' => 'Culturel',
             ],
             'type' => CategoryTypes::TOUR
         ]);
         Category::create([
             'name' => [
                 'en' => 'Others',
-                'fr' => 'Others (fr)',
+                'fr' => 'Autres',
             ],
             'type' => CategoryTypes::TOUR
         ]);
