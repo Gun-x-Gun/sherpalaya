7594678ea569cb5a81880e27ed6484504dc4bbb6
Umanga Pathak <umangapathak999@gmail.com>
2025-03-22 19:17:00 +0545

    translation utility class updates

--- app/Filament/Fields/TranslatableRepeater.php
+++ app/Filament/Fields/TranslatableRepeater.php
@@ -4,9 +4,18 @@ namespace App\Filament\Fields;
 
 use Closure;
 use Exception;
+use Filament\Actions\ActionGroup;
+use Filament\Actions\LocaleSwitcher;
+use Filament\Actions\SelectAction;
+use Filament\Forms\Components\Actions\Action;
 use Filament\Forms\Components\Field;
 use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\Tabs\Tab;
 use Filament\Support\Concerns\EvaluatesClosures;
+use Filament\Support\Enums\ActionSize;
+use Icetalker\FilamentTableRepeater\Forms\Components\TableRepeater;
+use Illuminate\Support\Arr;
 
 /**
 Repeater::make('costs_include')
@@ -30,14 +39,37 @@ class TranslatableRepeater
     use EvaluatesClosures;
 
     protected ?Closure $getFieldUsing = null;
+    protected ?Closure $getFieldsUsing = null;
+    protected ?Closure $getRepeaterUsing = null;
+
+    protected array $excludeFieldsFromTranslation = [];
 
     public static function make(string $name)
     {
         return new static($name);
     }
 
-    public function field(Closure $field): static{
+    public function field(Closure $field): static
+    {
         $this->getFieldUsing = $field;
+        return $this;
+    }
+
+    public function fields(Closure $field): static
+    {
+        $this->getFieldsUsing = $field;
+        return $this;
+    }
+
+    public function excludeFieldsFromTranslation(array $fields): static
+    {
+        $this->excludeFieldsFromTranslation = $fields;
+        return $this;
+    }
+
+    public function repeater(Closure $repeater): static
+    {
+        $this->getRepeaterUsing = $repeater;
         return $this;
     }
 
@@ -45,35 +77,163 @@ class TranslatableRepeater
         protected string $name,
     ) {}
 
-    public function simple(string $field): Repeater
+    protected function validateFields(array $fields): void
+    {
+        $fields = array_unique($fields);
+        $fields = array_filter($fields);
+        array_map(function ($field) {
+            return $this->validateField($field);
+        }, $fields);
+    }
+
+    protected function resolveFields(array $fields, array $columns): array
     {
 
-        if(!is_subclass_of($field, Field::class)){
-            throw new Exception(Field::class . " expected. Got " . $field);
+        if (count($fields) != count($columns)) {
+            throw new Exception("Unequal number of columns and fields for table repeater");
         }
 
+        $enFields = Arr::map($fields, function ($field, $key) use ($columns) {
+            if (in_array($columns[$key], $this->excludeFieldsFromTranslation)) {
+                return $field::make($columns[$key]);
+            } else {
+                return $field::make($columns[$key] . '.en');
+            }
+        });
+
+        $frFields = Arr::map($fields, function ($field, $key) use ($columns) {
+            if (in_array($columns[$key], $this->excludeFieldsFromTranslation)) {
+                return $field::make($columns[$key]);
+            } else {
+                return $field::make($columns[$key] . '.fr');
+            }
+        });
+
+        $getFieldsUsing = $this->getFieldsUsing;
+
+        if (!is_null($getFieldsUsing)) {
+            $enFields = $getFieldsUsing($enFields);
+            $frFields = $getFieldsUsing($frFields);
+        }
+
+        $enFields = Arr::map($enFields, function ($field, $key) use ($columns) {
+            return $field;
+        });
+
+        $frFields = Arr::map($frFields, function ($field, $key) use ($columns) {
+            return $field;
+        });
+
+        return [
+            'en' => $enFields,
+            'fr' => $frFields,
+        ];
+    }
+
+    protected function resolveRepeaters(array $fields): array
+    {
+
+        $enTableRepeater = TableRepeater::make($this->name);
+
+        $frTableRepeater = TableRepeater::make($this->name);
+
+        $getRepeaterUsing = $this->getRepeaterUsing;
+
+        if (!is_null($getRepeaterUsing)) {
+            $enTableRepeater = $getRepeaterUsing($enTableRepeater);
+            $frTableRepeater = $getRepeaterUsing($frTableRepeater);
+        }
+
+
+        $enTableRepeater = $enTableRepeater
+            ->afterStateHydrated(function ($state, $record, $component) {
+                // dd($state, $record, $component);
+            })
+            ->schema($fields['en'])
+            ->dehydrated(false);
+
+        $frTableRepeater = $frTableRepeater
+            ->afterStateHydrated(function ($state) {})
+            ->schema($fields['fr'])
+            ->dehydrated(false);
+
+        return [
+            'en' => $enTableRepeater,
+            'fr' => $frTableRepeater,
+        ];
+    }
+
+
+    protected function validateField(string $field): void
+    {
+        if (!is_subclass_of($field, Field::class)) {
+            throw new Exception(Field::class . " expected. Got " . $field);
+        }
+    }
+
+    protected function resolveField(string $field): array
+    {
         $enField = $field::make('en');
 
         $frField = $field::make('fr');
 
         $getFieldUsing = $this->getFieldUsing;
 
-        if(!is_null($getFieldUsing)){
+        if (!is_null($getFieldUsing)) {
             $enField = $getFieldUsing($enField);
             $frField = $getFieldUsing($frField);
         }
+
+        return [
+            'en' => $enField,
+            'fr' => $frField,
+        ];
+    }
+
+    public function simple(string $field): Repeater
+    {
+
+        $this->validateField($field);
+        $fields = $this->resolveField($field);
+
 
         return Repeater::make($this->name)
             ->afterStateHydrated(function ($component, $state) {
                 $component->state($state['en']);
             })
-            ->dehydrateStateUsing(function($state){
+            ->dehydrateStateUsing(function ($state) {
                 // dd($state);
                 return $state;
             })
             ->schema([
-                $enField,
-                $frField,
+                $fields['en'],
+                $fields['fr'],
+            ]);
+    }
+
+    public function table(array $fields, array $columns): Tabs
+    {
+        $this->validateFields($fields);
+
+        $fields = $this->resolveFields($fields, $columns);
+
+        $repeaters = $this->resolveRepeaters($fields);
+
+        return Tabs::make()
+            ->dehydrated(true)
+            ->dehydrateStateUsing(function ($state) {
+                dd($state);
+                return $state;
+            })
+            ->schema([
+                Tab::make('English')
+                    ->schema([
+                        $repeaters['en']
+                    ]),
+                Tab::make('French')
+                    ->schema([
+                        $repeaters['fr']
+                    ]),
             ]);
     }
 }
