ab38a4e320bf8afa600138e93768b2f5f5727125
Adesh Bhattarai <adeshbhattarai99@gmail.com>
2025-03-27 14:59:02 +0545

    resource

--- app/Filament/Resources/TrekResource.php
+++ app/Filament/Resources/TrekResource.php
@@ -370,11 +370,6 @@ class TrekResource extends Resource
                             ->icon('heroicon-m-clock')
                             ->size(TextColumn\TextColumnSize::Small)
                             ->suffix(' days'),
-                        TextColumn::make('category.name')
-                            ->badge()
-                            ->icon('heroicon-m-bolt')
-                            ->size(TextColumn\TextColumnSize::Small)
-                            ->prefix(''),
                         TextColumn::make('region.name')
                             ->badge()
                             ->icon('heroicon-m-map')
