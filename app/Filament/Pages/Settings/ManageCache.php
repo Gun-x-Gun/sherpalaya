e181a8134bd156df17effc45357651c07bb29f59
Umanga Pathak <umangapathak999@gmail.com>
2025-03-27 15:53:24 +0545

    reverted import

--- app/Filament/Pages/Settings/ManageCache.php
+++ app/Filament/Pages/Settings/ManageCache.php
@@ -9,7 +9,7 @@ use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
 use Filament\Notifications\Notification;
 use Filament\Pages\Page;
-use LSCache;
+use Litespeed\LSCache\LSCache;
 
 class ManageCache extends Page
 {
