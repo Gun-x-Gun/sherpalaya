82b2b5b8319088c45e82f506be2d9c07960b402e
Subeen928 <subintmg321@gmail.com>
2025-03-31 17:55:22 +0545

    booking section whatsapp

--- resources/views/components/booking/booking-section.blade.php
+++ resources/views/components/booking/booking-section.blade.php
@@ -18,8 +18,13 @@
     </div>
 </div>
 
-<a href="{{ $bookingFor->getWhatsappUrl() }}" class="fixed bottom-5 right-10 z-auto text-right " target="_blank">
-    <span class="icon-[ph--whatsapp-logo-duotone] size-15 text-green-600 hover:size-16"></span>
+<a href="{{ $bookingFor->getWhatsappUrl() }}" class="fixed bottom-5 right-4 z-50 bg-opacity-80 " target="_blank">
+    <div class="flex flex-col justify-center items-center">
+        <span class="icon-[ph--whatsapp-logo-duotone] size-15 text-green-500 hover:size-16 hover:text-green-600"></span>
+        <span class="glass rounded-lg px-2 ">Click to Connect
+
+        </span>
+    </div>
 </a>
 
 @push('modals')
