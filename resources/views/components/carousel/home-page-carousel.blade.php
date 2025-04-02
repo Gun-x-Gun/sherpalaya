21f75c66c0d95c44aab3f79a3755961f0f536da0
Subeen928 <subintmg321@gmail.com>
2025-04-02 16:52:25 +0545

    homepage-carousel update

--- resources/views/components/carousel/home-page-carousel.blade.php
+++ resources/views/components/carousel/home-page-carousel.blade.php
@@ -7,8 +7,38 @@
                 <!-- Slide 1 -->
                 @foreach ($featuredData as $featured)
                     <div class="carousel-slide snap-center rounded-none ">
-                        <x-cards.carousel-card :id="$featured->id" :title="$featured->title" :url="$featured->url" :image="$featured->image"
-                            :description="$featured->description" />
+                        {{-- <x-cards.carousel-card :id="$featured->id" :title="$featured->title" :url="$featured->url" :image="$featured->image"
+                            :description="$featured->description" /> --}}
+
+                        <div class="card--rounded-none image-full  h-dvh relative ">
+                            {{-- <x-curator-glider class="h-dvh w-full object-cover brightness-75" :media="$image" :fallback="asset('/photos/banner.jpg')"
+                                        loading="lazy" alt="{{ $title }} Image" /> --}}
+                            <figure class="h-dvh w-full bg-gray-600">
+                                <img src="{{ optional($featured->image)->url ?? asset('photos/DSCF2600.JPG') }}"
+                                    alt="{{ $featured->title }} Cover Image"
+                                    class="brightness-75 h-dvh w-full object-cover" />
+                            </figure>
+                            <div class="card-body absolute inset-0 flex items-center justify-start leading-[1.1] md:leading-snug"
+                                data-aos="fade-down" data-aos-duration="1500">
+                                <div class="absolute bottom-2/4 left-4 xl:left-32 transform translate-y-1/2 ">
+                                    <h2
+                                        class="card-title mb-2 text-warning text-4xl sm:text-5xl md:text-6xl  uppercase font-card font-semibold tracking-tight text-preety md:w-[70%] leading-[1.3]  overflow-hidden opacity-75">
+                                        {{ $featured->title }}
+                                    </h2>
+                                    <h5
+                                        class="card-title  mb-8 text-blue-50 text-4xl sm:text-5xl md:text-6xl  uppercase font-card font-semibold tracking-wide text-wrap md:w-[70%] leading-[1.3]  overflow-hidden opacity-75">
+                                        {{ $featured->description }}
+                                    </h5>
+                                    <a href="{{ $featured->url }}">
+                                        <button
+                                            class="btn btn-primary btn-lg  gap-2 rounded-full text-base hover:btn-warning tracking-wide pl-2 opacity-80">
+                                            <span class="icon-[mdi--chevron-double-right] size-5 "></span>
+                                            {{ __('home-carousel.explore') }}
+                                        </button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 @endforeach
             </div>
